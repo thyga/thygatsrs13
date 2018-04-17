da429899d131dcb3983953553e83da94c1e5c99f
bazard <bazard@foutue.fr>
2018-04-17 09:48:43 +0200

    test

---
+++ scripts2.ps1
@@ -0,0 +1,58 @@
+﻿<#
+$user = @{Nom="Dan"; Prénom="Thierry"; age=32; Role="student"}
+
+
+$user["Nom"]
+ 
+foreach($item in $fruits) {
+      $item
+      write-host "je tourne"
+}#>
+<#
+for($i = 0; $i -lt 10; $i++){
+    $fruits[$i]
+
+        
+}#>
+$i = 0;
+<#
+while ($i -lt 10) {
+write-output $i;
+$i++;
+}#>
+
+$a =0
+<#
+while($a -lt 11){
+   $a * 5
+   $a++
+   }#>
+<#   
+while($a -lt 21){
+$a *10
+$a++
+}#>
+<#
+while($a -lt 5){
+$a * 15
+$a++
+}#>
+
+function hello($name)
+{
+   "hello " + [string]$name
+}
+hello("thierry")
+hello("toto")
+
+function world
+{  
+$w = "world"
+hello($w)
+}
+world
+
+
+
+
+
