9bfb27a7f8e689ee4927e498467db7635f4e5c8d
thyga <38430426+thyga@users.noreply.github.com>
2018-04-19 14:00:48 +0200

    Add files via upload

---
+++ exercice.py
@@ -0,0 +1,119 @@
+'''
+def ex1():
+    text = "je dois faire des sauvegardes régulières de mes fichiers"
+    e = 0
+    while e < 500:
+        print(text)
+        e = e + 1
+ex1()
+'''
+
+'''
+ def ex2():
+        r = ""
+        for i in range(n1, n2 + 1000):
+            if i % 2:
+                r + = str(i) + " "
+        return r
+ex2 ()
+'''
+
+'''
+def ex5():
+    a = int(input( " entrez un nombre : "))
+    if (a % 2 == 0):
+    print("pair")
+    else:
+    print("impair")
+ex5()
+'''
+
+'''
+def ex3():
+    compteur = 1
+    while compteur <=10:
+        print(compteur, "*13 =", compteur *13)
+        compteur +=1
+ex3
+'''
+'''
+def ex4
+    print = ("donne moi un mot")
+    print = (len(valeur))
+ex4
+    
+'''
+'''
+
+def ex10():
+    nbr = int(input("donne l'âge?:"))
+    if (nbr == 6 or nbr == 7)
+          print ("poussin")
+   elif(nbr == 8 or nbr == 9)
+          print ("pupille")
+   elif(nbr == 10 or nbr == 11)
+          print ("minime")
+   elif(nbr > 12)
+          print ("cadet")
+   else
+          print ("too small")
+ex10
+'''
+
+'''
+def ex6():
+     nbr = int(input("ecrire un nombre entier entre 10 et 20 :"))
+    if (nbr > 20):
+      print text ("inferieur")
+    elif(nbr <10):
+      print text ("superieur")
+ex6()
+'''
+'''
+def ex7():
+    nbr = int(input("ecrire un nombre :"))
+    print(nbr + 1)
+    print(nbr + 2)
+    print(nbr + 3)
+    print(nbr + 4)
+    print(nbr + 5)
+    print(nbr + 6)
+    print(nbr + 7)
+    print(nbr + 8)
+    print(nbr + 9)
+    print(nbr + 10)
+ex7()
+'''
+'''
+def ex8():
+    nbr = int(input("ecrire un nombre :"))
+    print(nbr * 1)
+    print(nbr * 2)
+    print(nbr * 3)
+    print(nbr * 4)
+    print(nbr * 5)
+    print(nbr * 6)
+    print(nbr * 7)
+    print(nbr * 8)
+    print(nbr * 9)
+    print(nbr * 10)
+ex8()
+'''
+'''
+ def ex9():
+     age = int(input("quel est l'age de votre enfant?"))
+ex9()
+'''
+'''
+def ex11():
+    nbr_article = int(input("nbre d'article pose?:"))
+    prix _article = float(input("quel est le prix HT unitaire de l'article?:"))
+    prixht = prix article * nbr_article
+    print ("nombres d'articles : ",nbr_article)
+    print ("prix HT : ",printht)
+    tva = (prixht * 1.2)
+    print ("prix TTC:", tva)
+ex11
+'''
+
+
